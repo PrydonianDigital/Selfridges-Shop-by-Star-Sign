662cfb845d61533fa08293200f48200195a71b63
vbloke <vbloke@gmail.com>
2015-10-06 08:51:06 +0100

    initial import

---
+++ female.js
@@ -0,0 +1,602 @@
+{
+	"name": "female",
+	"starsigns": [
+		{
+				"id": "0",
+				"name": "Aries",
+				"url": "",
+				"slides": [
+					{
+						"id": "1",
+						"name": "Aries",
+						"birthdate": "Aries",
+						"constellation": "Aries",
+						"signText": "If the Aries in your life hasn't already taken charge and let you know precisely what she wants, we suggest you double-check her date of birth. However, it's possible she's been too busy taking over the world, so if the task of choosing does fall to you – just ask. Aries is nothing if not direct.",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "2",
+						"name": "Aries",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "With Mercury in their house of authority, Aries will want to take charge this Christmas – as usual. But with generous Jupiter lighting up their sixth house of selfless service, they’ll offer to do every little job – from doing the washing up to dishing out the gifts.",
+						"embed": "188556035",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "3",
+						"name": "Aries",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "Aries",
+						"icon": "Aries",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "4",
+						"name": "Aries",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "Aries",
+						"planet": "Mars",
+						"planetText": "Aries' passion and drive is directed by the planet Mars, the ruler of all things fearsome and feisty. Mars governs competition in all its forms – from sports to all-out war – and when it comes to gifting, be sure to reciprocate Aries' show of generosity if you want to avoid any showdowns over the Christmas pud.",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "5",
+						"name": "Aries",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "Aries",
+						"colour": "Aries",
+						"colourText": "Aries",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "6",
+						"name": "Aries",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "Aries",
+						"bodyImage": "Aries",
+						"bodyText": "This sign charges at life head-on, in a bid to always be first off the starting blocks."
+					}
+				]
+			},
+		{
+				"id": "1",
+				"name": "Taurus",
+				"url": "",
+				"slides": [
+					{
+						"id": "1",
+						"name": "Taurus",
+						"birthdate": "Taurus",
+						"constellation": "Taurus",
+						"signText": "Sensuous Taurus lives for the luxuries of life, but beware: glitz and glamour alone will leave her cold. Instead, seek out gifts that appeal to all her senses, enveloping her in a cocoon of comfort. Think unusual jewellery or lashings of lavish bathroom treats.",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "2",
+						"name": "Taurus",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "Taurus will take centre stage this Christmas, with expansive, outgoing Jupiter cruising through their attention-seeking fifth house. And with Mercury in the area of their chart ruling storytelling, let them entertain the gang with funny tales of Christmas past.",
+						"embed": "188556035",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "3",
+						"name": "Taurus",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "Taurus",
+						"icon": "Taurus",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "4",
+						"name": "Taurus",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "Taurus",
+						"planet": "Venus",
+						"planetText": "Venus governs the rules of attraction, and her Taurean subjects are absolute suckers for the beautiful things in life – not to mention the riches it often takes to purchase them. When Venus is in town, luxury and charm are the order of the day.",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "5",
+						"name": "Taurus",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "Taurus",
+						"colour": "Taurus",
+						"colourText": "Taurus",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "6",
+						"name": "Taurus",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "Taurus",
+						"bodyImage": "Taurus",
+						"bodyText": "This sign charges at life head-on, in a bid to always be first off the starting blocks."
+					}
+				]
+			},
+		{
+				"id": "2",
+				"name": "Gemini",
+				"url": "",
+				"slides": [
+					{
+						"id": "1",
+						"name": "Gemini",
+						"birthdate": "Gemini",
+						"constellation": "Gemini",
+						"signText": "Sensuous Gemini lives for the luxuries of life, but beware: glitz and glamour alone will leave her cold. Instead, seek out gifts that appeal to all her senses, enveloping her in a cocoon of comfort. Think unusual jewellery or lashings of lavish bathroom treats.",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "2",
+						"name": "Gemini",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "Gemini will take centre stage this Christmas, with expansive, outgoing Jupiter cruising through their attention-seeking fifth house. And with Mercury in the area of their chart ruling storytelling, let them entertain the gang with funny tales of Christmas past.",
+						"embed": "188556035",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "3",
+						"name": "Gemini",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "Gemini",
+						"icon": "Gemini",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "4",
+						"name": "Gemini",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "Gemini",
+						"planet": "Saturn",
+						"planetText": "Venus governs the rules of attraction, and her Taurean subjects are absolute suckers for the beautiful things in life – not to mention the riches it often takes to purchase them. When Venus is in town, luxury and charm are the order of the day.",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "5",
+						"name": "Gemini",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "Gemini",
+						"colour": "Gemini",
+						"colourText": "Gemini",
+						"bodyTitle": "",
+						"bodyImage": "",
+						"bodyText": ""
+					},
+					{
+						"id": "6",
+						"name": "Gemini",
+						"birthdate": "",
+						"constellation": "",
+						"signText": "",
+						"horoscope": "",
+						"embed": "",
+						"iconTitle": "",
+						"icon": "",
+						"planetTitle": "",
+						"planet": "",
+						"planetText": "",
+						"colourTitle": "",
+						"colour": "",
+						"colourText": "",
+						"bodyTitle": "Gemini",
+						"bodyImage": "Gemini",
+						"bodyText": "This sign charges at life head-on, in a bid to always be first off the starting blocks."
+					}
+				]
+			},
+		{
+				"id": "3",
+				"name": "Cancer",
+				"slides": [
+					{
+						"id": 1
+					},
+					{
+						"id": 2
+					},
+					{
+						"id": 3
+					},
+					{
+						"id": 4
+					},
+					{
+						"id": 5
+					},
+					{
+						"id": 6
+					}
+				]
+			},
+		{
+				"id": "4",
+				"name": "Leo",
+				"slides": [
+					{
+						"id": 1
+					},
+					{
+						"id": 2
+					},
+					{
+						"id": 3
+					},
+					{
+						"id": 4
+					},
+					{
+						"id": 5
+					},
+					{
+						"id": 6
+					}
+				]
+			},
+		{
+				"id": "5",
+				"name": "Virgo",
+				"slides": [
+					{
+						"id": 1
+					},
+					{
+						"id": 2
+					},
+					{
+						"id": 3
+					},
+					{
+						"id": 4
+					},
+					{
+						"id": 5
+					},
+					{
+						"id": 6
+					}
+				]
+			},
+		{
+				"id": "6",
+				"name": "Libra",
+				"slides": [
+					{
+						"id": 1
+					},
+					{
+						"id": 2
+					},
+					{
+						"id": 3
+					},
+					{
+						"id": 4
+					},
+					{
+						"id": 5
+					},
+					{
+						"id": 6
+					}
+				]
+			},
+		{
+				"id": "7",
+				"name": "Scorpio",
+				"slides": [
+					{
+						"id": 1
+					},
+					{
+						"id": 2
+					},
+					{
+						"id": 3
+					},
+					{
+						"id": 4
+					},
+					{
+						"id": 5
+					},
+					{
+						"id": 6
+					}
+				]
+			},
+		{
+				"id": "8",
+				"name": "Sagittarius",
+				"slides": [
+					{
+						"id": 1
+					},
+					{
+						"id": 2
+					},
+					{
+						"id": 3
+					},
+					{
+						"id": 4
+					},
+					{
+						"id": 5
+					},
+					{
+						"id": 6
+					}
+				]
+			},
+		{
+				"id": "9",
+				"name": "Capricorn",
+				"slides": [
+					{
+						"id": 1
+					},
+					{
+						"id": 2
+					},
+					{
+						"id": 3
+					},
+					{
+						"id": 4
+					},
+					{
+						"id": 5
+					},
+					{
+						"id": 6
+					}
+				]
+			},
+		{
+				"id": "10",
+				"name": "Aquarius",
+				"slides": [
+					{
+						"id": 1
+					},
+					{
+						"id": 2
+					},
+					{
+						"id": 3
+					},
+					{
+						"id": 4
+					},
+					{
+						"id": 5
+					},
+					{
+						"id": 6
+					}
+				]
+			},
+		{
+				"id": "11",
+				"name": "Pisces",
+				"slides": [
+					{
+						"id": 1
+					},
+					{
+						"id": 2
+					},
+					{
+						"id": 3
+					},
+					{
+						"id": 4
+					},
+					{
+						"id": 5
+					},
+					{
+						"id": 6
+					}
+				]
+			}
+	]
+}
---
+++ sbss.js
@@ -0,0 +1,49 @@
+{
+	"Name": "Shop By Star Sign",
+	"Version": 1,
+	"Authors": [
+		{
+			"name": "Mark Duwe",
+			"email": "mark@prydonian.digital"
+		},
+		{
+			"name": "Rob Young",
+			"email": "rob.young@selfridges.co.uk"
+		}
+	],
+	"slides": [
+		{
+			"ids": "1",
+			"name": "homeSlide",
+			"header": "images/sbss_header.png",
+			"headerId": "mainTitle",
+			"image": "images/sbss_logo.png",
+			"imageId": "mainLogo",
+			"bg": "images/sbss_bg.jpg",
+			"text": "",
+			"data": [
+				{}
+			]
+		},
+		{
+			"ids": "2",
+			"name": "selectSlide",
+			"header": "images/sbss_menu_title.png",
+			"headerId": "mainTitle",
+			"image": "",
+			"imageId": "",
+			"bg": "images/sbss_bg.jpg",
+			"text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+			"data": [
+				{}
+			]
+		}
+	],
+	"gender": [
+		{
+			"name": "female"
+		}, {
+			"name": "male"
+		}
+	]
+}
