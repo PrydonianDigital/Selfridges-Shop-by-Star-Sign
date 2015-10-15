9309989e00a08a62103b833ff79a8d8fa5f4feb3
vbloke <vbloke@gmail.com>
2015-10-15 09:17:07 +0100

    changed file paths

--- sbss.js
+++ sbss.js
@@ -17,9 +17,9 @@
 			"name": "homeSlide",
 			"header": "",
 			"headerId": "",
-			"image": "images/sbss_logo.png",
+			"image": "sbss_logo.png",
 			"imageId": "mainLogo",
-			"bg": "images/sbss_bg.jpg",
+			"bg": "sbss_bg.jpg",
 			"text": "",
 			"data": [
 				{}
@@ -28,12 +28,13 @@
 		{
 			"ids": "2",
 			"name": "selectSlide",
-			"header": "images/sbss_menu_title.png",
+			"header": "sbss_menu_title.png",
 			"headerId": "mainTitle",
 			"image": "",
 			"imageId": "",
-			"bg": "images/sbss_bg.jpg",
-			"text": "Your search for the perfect gift is written in the stars this Christmas. take a cosmic journey with The Numinous - the blogosphere's leading voice on style and mysticism - and discover our handpicked edit of gifts by star sign, plus plenty of celestial guidance, festive forecasts and more along the way.",
+			"bg": "sbss_bg.jpg",
+			"text": "Your search for the perfect gift is written in the stars this Christmas. Take a cosmic journey with The Numinous - the blogosphere's leading voice on style and mysticism - and discover our handpicked edit of gifts by star sign, plus plenty of celestial guidance, festive forecasts and more along the way. Select your gender and star sign to begin...",
+			"mobiletext": "The perfect gift is written in the stars this Christmas. Take a cosmic journey with The Numinous - the blogosphere's leading voice on style and mysticism - and discover our pick of gifts by star sign, plus plenty of celestial guidance along the way. Select your gender and star sign to begin...",
 			"data": [
 				{}
 			]
